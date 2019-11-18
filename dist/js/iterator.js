3df4dafced985a72df7c160e0eebf84e22358be7
Molchanova Tatyana <mtuspeh@gmail.com>
2019-11-18 09:08:03 +0200

    n commit

--- dist/js/iterator.js
+++ dist/js/iterator.js
@@ -1,38 +1,226 @@
 'use strict';
 
-var xmen = ['Cyclopes', 'Wolverine', 'Rogue'];
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-// es5 cycles
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-// console.info('for');
-// for (let index = 0; index < xmen.length; index++) {
-//     console.log(xmen[index]);
+// part 1
+
+// let xmen = ['Cyclopes', 'Wolverine', 'Rogue'];
+
+// // es5 cycles
+
+// // console.info('for');
+// // for (let index = 0; index < xmen.length; index++) {
+// //     console.log(xmen[index]);
+// // }
+
+// // console.info('for...in');
+// // for (let key in xmen) {
+// //     console.log(xmen[key]);
+// // }
+
+// // console.info('forEach');
+// // xmen.forEach(xman => console.log(xman));
+
+// // es6 cycle
+
+// console.info('for...of');
+// for (let xman of xmen) {
+//     console.log(xman);
 // }
 
-// console.info('for...in');
-// for (let key in xmen) {
-//     console.log(xmen[key]);
+// // console.log(typeof xmen[Symbol.iterator]);
+// // console.log(xmen[Symbol.iterator]());
+
+// let iterator = xmen[Symbol.iterator]();
+
+// // console.log(iterator.next());
+// // console.log(iterator.next());
+// // console.log(iterator.next());
+// // console.log(iterator.next());
+// // console.log(iterator.next());
+
+// // console.log(iterator.length); //undefined
+
+// let next = iterator.next();
+
+// while (!next.done) {
+//     console.log(next.value);
+//     next = iterator.next();
 // }
 
-// console.info('forEach');
-// xmen.forEach(xman => console.log(xman));
 
-// es6 cycle
+// // 6:51 https://www.youtube.com/watch?v=3LDvVylk-NU
 
-console.info('for...of');
+// part 2
+
+// let idGenerator = {
+//     [Symbol.iterator]() {
+//         let id = 1;
+//         return {
+//             next() {
+//                 // let value = id++;
+//                 let value = id > 100 ? undefined : id++;
+//                 // let done = false;
+//                 let done = !value;
+
+//                 return { value, done };
+
+//                 // return {
+//                 //     value: undefined,
+//                 //     done: true
+//                 // };
+//             }
+//         };
+//     }
+// };
+
+// for (let id of idGenerator) {
+//     // console.log(id);
+//     // if (id > 100) {
+//     //     break;
+//     // }
+//     console.log(id);
+// }
+
+// Generator of random numbers
+
+// let randomGenerator = {
+//     generate() {
+//         return this[Symbol.iterator]();
+//     },
+//     [Symbol.iterator]() {
+//         let count = 0;
+//         return {
+//             next() {
+//                 let value = Math.ceil(Math.random() * 100);
+//                 let done = count > 9;
+//                 // console.log(done);
+//                 count += 1;
+//                 return { value, done };
+//             }
+//         };
+//     }
+// };
+
+// // for (let random of randomGenerator) {
+// //     console.log(random);
+// // }
+
+// // let random = randomGenerator[Symbol.iterator]();
+// let random = randomGenerator.generate();
+// console.log(random.next().value);
+
+// part 3
+
+// class TaskList {
+//     constructor() {
+//         this.tasks = [];
+//     }
+
+//     addTasks(...tasks) {
+//         this.tasks = this.tasks.concat(tasks);
+//     }
+
+//     [Symbol.iterator]() {
+//         let tasks = this.tasks;
+//         let index = 0;
+
+//         return {
+//             next() {
+//                 let result = { value: undefined, done: true };
+
+//                 if (index < tasks.length) {
+//                     result.value = tasks[index];
+//                     console.log(tasks[index]);
+//                     result.done = false;
+//                     index += 1;
+//                 }
+//                 return result;
+//             } 
+//         };
+
+
+//     }
+// }
+
+// let taskList = new TaskList();
+// taskList.addTasks('Learn JS', 'Learn ES6', 'Buy products');
+
+// for (let task of taskList) {
+//     console.log(task);
+// }
+
+
+// part 4
+
+var ArrayIterator = function () {
+    function ArrayIterator(array) {
+        _classCallCheck(this, ArrayIterator);
+
+        this.array = array.map(function (item) {
+            return item;
+        }).sort();
+        this.index = 0;
+    }
+
+    _createClass(ArrayIterator, [{
+        key: 'next',
+        value: function next() {
+            var result = { value: undefined, done: true };
+
+            if (this.index < this.array.length) {
+                result.value = this.array[this.index];
+                result.done = false;
+                this.index += 1;
+            }
+            return result;
+        }
+    }]);
+
+    return ArrayIterator;
+}();
+
+var TaskList = function () {
+    function TaskList() {
+        _classCallCheck(this, TaskList);
+
+        this.tasks = [];
+    }
+
+    _createClass(TaskList, [{
+        key: 'addTasks',
+        value: function addTasks() {
+            for (var _len = arguments.length, tasks = Array(_len), _key = 0; _key < _len; _key++) {
+                tasks[_key] = arguments[_key];
+            }
+
+            this.tasks = this.tasks.concat(tasks);
+        }
+    }, {
+        key: Symbol.iterator,
+        value: function value() {
+            return new ArrayIterator(this.tasks);
+        }
+    }]);
+
+    return TaskList;
+}();
+
+var taskList = new TaskList();
+taskList.addTasks('Learn JS', 'Learn ES6', 'Buy products');
+
 var _iteratorNormalCompletion = true;
 var _didIteratorError = false;
 var _iteratorError = undefined;
 
 try {
-    for (var _iterator = xmen[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-        var xman = _step.value;
+    for (var _iterator = taskList[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var task = _step.value;
 
-        console.log(xman);
+        console.log(task);
     }
-
-    // console.log(typeof xmen[Symbol.iterator]);
-    // console.log(xmen[Symbol.iterator]());
 } catch (err) {
     _didIteratorError = true;
     _iteratorError = err;
@@ -47,22 +235,3 @@ try {
         }
     }
 }
-
-var iterator = xmen[Symbol.iterator]();
-
-// console.log(iterator.next());
-// console.log(iterator.next());
-// console.log(iterator.next());
-// console.log(iterator.next());
-// console.log(iterator.next());
-
-// console.log(iterator.length); //undefined
-
-var next = iterator.next();
-
-while (!next.done) {
-    console.log(next.value);
-    next = iterator.next();
-}
-
-// 6:51 https://www.youtube.com/watch?v=3LDvVylk-NU
--- src/js/iterator.js
+++ src/js/iterator.js
@@ -1,46 +1,192 @@
-let xmen = ['Cyclopes', 'Wolverine', 'Rogue'];
+// part 1
 
-// es5 cycles
+// let xmen = ['Cyclopes', 'Wolverine', 'Rogue'];
 
-// console.info('for');
-// for (let index = 0; index < xmen.length; index++) {
-//     console.log(xmen[index]);
+// // es5 cycles
+
+// // console.info('for');
+// // for (let index = 0; index < xmen.length; index++) {
+// //     console.log(xmen[index]);
+// // }
+
+// // console.info('for...in');
+// // for (let key in xmen) {
+// //     console.log(xmen[key]);
+// // }
+
+// // console.info('forEach');
+// // xmen.forEach(xman => console.log(xman));
+
+// // es6 cycle
+
+// console.info('for...of');
+// for (let xman of xmen) {
+//     console.log(xman);
 // }
 
-// console.info('for...in');
-// for (let key in xmen) {
-//     console.log(xmen[key]);
+// // console.log(typeof xmen[Symbol.iterator]);
+// // console.log(xmen[Symbol.iterator]());
+
+// let iterator = xmen[Symbol.iterator]();
+
+// // console.log(iterator.next());
+// // console.log(iterator.next());
+// // console.log(iterator.next());
+// // console.log(iterator.next());
+// // console.log(iterator.next());
+
+// // console.log(iterator.length); //undefined
+
+// let next = iterator.next();
+
+// while (!next.done) {
+//     console.log(next.value);
+//     next = iterator.next();
 // }
 
-// console.info('forEach');
-// xmen.forEach(xman => console.log(xman));
 
-// es6 cycle
+// // 6:51 https://www.youtube.com/watch?v=3LDvVylk-NU
 
-console.info('for...of');
-for (let xman of xmen) {
-    console.log(xman);
+// part 2
+
+// let idGenerator = {
+//     [Symbol.iterator]() {
+//         let id = 1;
+//         return {
+//             next() {
+//                 // let value = id++;
+//                 let value = id > 100 ? undefined : id++;
+//                 // let done = false;
+//                 let done = !value;
+
+//                 return { value, done };
+
+//                 // return {
+//                 //     value: undefined,
+//                 //     done: true
+//                 // };
+//             }
+//         };
+//     }
+// };
+
+// for (let id of idGenerator) {
+//     // console.log(id);
+//     // if (id > 100) {
+//     //     break;
+//     // }
+//     console.log(id);
+// }
+
+// Generator of random numbers
+
+// let randomGenerator = {
+//     generate() {
+//         return this[Symbol.iterator]();
+//     },
+//     [Symbol.iterator]() {
+//         let count = 0;
+//         return {
+//             next() {
+//                 let value = Math.ceil(Math.random() * 100);
+//                 let done = count > 9;
+//                 // console.log(done);
+//                 count += 1;
+//                 return { value, done };
+//             }
+//         };
+//     }
+// };
+
+// // for (let random of randomGenerator) {
+// //     console.log(random);
+// // }
+
+// // let random = randomGenerator[Symbol.iterator]();
+// let random = randomGenerator.generate();
+// console.log(random.next().value);
+
+// part 3
+
+// class TaskList {
+//     constructor() {
+//         this.tasks = [];
+//     }
+
+//     addTasks(...tasks) {
+//         this.tasks = this.tasks.concat(tasks);
+//     }
+
+//     [Symbol.iterator]() {
+//         let tasks = this.tasks;
+//         let index = 0;
+
+//         return {
+//             next() {
+//                 let result = { value: undefined, done: true };
+
+//                 if (index < tasks.length) {
+//                     result.value = tasks[index];
+//                     console.log(tasks[index]);
+//                     result.done = false;
+//                     index += 1;
+//                 }
+//                 return result;
+//             } 
+//         };
+
+
+//     }
+// }
+
+// let taskList = new TaskList();
+// taskList.addTasks('Learn JS', 'Learn ES6', 'Buy products');
+
+// for (let task of taskList) {
+//     console.log(task);
+// }
+
+
+// part 4
+
+class ArrayIterator {
+    constructor(array) {
+        this.array = array.map(item => item).sort();
+        this.index = 0;
+    }
+
+    next() {
+        let result = { value: undefined, done: true };
+
+        if (this.index < this.array.length) {
+            result.value = this.array[this.index];
+            result.done = false;
+            this.index += 1;
+        }
+        return result;
+    }
 }
 
-// console.log(typeof xmen[Symbol.iterator]);
-// console.log(xmen[Symbol.iterator]());
+class TaskList {
+    constructor() {
+        this.tasks = [];
+    }
 
-let iterator = xmen[Symbol.iterator]();
+    addTasks(...tasks) {
+        this.tasks = this.tasks.concat(tasks);
+    }
 
-// console.log(iterator.next());
-// console.log(iterator.next());
-// console.log(iterator.next());
-// console.log(iterator.next());
-// console.log(iterator.next());
+    [Symbol.iterator]() {
+        return new ArrayIterator(this.tasks);
 
-// console.log(iterator.length); //undefined
 
-let next = iterator.next();
-
-while (!next.done) {
-    console.log(next.value);
-    next = iterator.next();
+    }
 }
 
+let taskList = new TaskList();
+taskList.addTasks('Learn JS', 'Learn ES6', 'Buy products');
 
-// 6:51 https://www.youtube.com/watch?v=3LDvVylk-NU
+for (let task of taskList) {
+    console.log(task);
+}
+
